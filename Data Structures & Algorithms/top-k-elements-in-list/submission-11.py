722079fec95f21381dd90df9cbc235d1dee821d7
Eduardo Carreño <73654460+EduardoCC-bot@users.noreply.github.com>
2026-06-16 12:28:06 -0600

    Add: top-k-elements-in-list - submission-11

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashMap = defaultdict(int)
+        res = []
+        for n in nums:
+            hashMap[n] += 1
+        
+        elementos_repetidos = sorted(hashMap.items(), key=lambda item : item[1], reverse = True)
+        for i in range(k):
+            res.append(elementos_repetidos[i][0])
+        return res
+
