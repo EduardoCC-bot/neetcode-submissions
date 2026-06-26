f270526a0cde37df57ea5758f9b3cb87d680fadb
Eduardo Carreño <73654460+EduardoCC-bot@users.noreply.github.com>
2026-06-25 18:51:55 -0600

    Add: buildings-with-an-ocean-view - submission-5

---
+++ Data Structures & Algorithms/buildings-with-an-ocean-view/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        res = []
+        l = 0
+        stack = []
+        for i in range(len(heights)):
+            while stack and heights[i] >= heights[stack[-1]]:
+                stack.pop()
+            stack.append(i)
+        return stack
