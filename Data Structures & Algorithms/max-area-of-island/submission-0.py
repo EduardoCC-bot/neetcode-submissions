2eb1e773868a47e11a26c2b1b0862b3bdd2286d1
Eduardo Carreño <73654460+EduardoCC-bot@users.noreply.github.com>
2026-07-27 00:41:33 -0600

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        """
+        My firts thoughts on this one it's use a DFS if the cell
+        it's 0 just move to the possible moves, have an set to save visit
+        cells,  
+        
+        """
+        visited = set()
+        maxArea = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        def dfs(r, c):
+            if min(r, c) < 0 or r >= ROWS or c >= COLS or (r,c) in visited or grid[r][c] == 0:
+                return 0
+            visited.add((r, c))
+            #Validate posible positions
+            curr = dfs(r + 1,c) + dfs(r - 1,c) +dfs(r,c + 1) +dfs(r,c - 1) + 1
+            return curr
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    area = dfs(r, c)
+                    maxArea = max(maxArea, area)
+
+        return maxArea
