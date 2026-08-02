0d365003fe1d5431847bb4fa749e9bc4bc0ba156
Eduardo Carreño <73654460+EduardoCC-bot@users.noreply.github.com>
2026-08-02 14:35:29 -0600

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        """
+        
+        """
+        r = x
+        while r * r > x:
+            r = (r + x // r) >> 1
+        return r
