6518cee80839225e02080ef1305354210700bdbc
Eduardo Carreño <73654460+EduardoCC-bot@users.noreply.github.com>
2026-08-24 21:06:56 -0600

    Add: find-the-difference - submission-1

---
+++ Data Structures & Algorithms/find-the-difference/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s = sorted(s)
+        t = sorted(t)
+
+        for i in range(len(t)):
+            if i < len(s) and s[i] != t[i]:
+                    return t[i]
+
+
+        return t[-1]   
