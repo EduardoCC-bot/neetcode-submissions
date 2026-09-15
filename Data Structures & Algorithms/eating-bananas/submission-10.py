8216fe7eaa633f36fdda08e3fd5f0d4183a8c642
Eduardo Carreño <73654460+EduardoCC-bot@users.noreply.github.com>
2026-09-14 23:23:24 -0600

    Add: eating-bananas - submission-10

---
+++ Data Structures & Algorithms/eating-bananas/submission-10.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        """
+        [1,4,3,2] h = 9
+
+        maximun is max of list
+        1    4
+        4 hours
+        4 // 2 = 2
+        7 hours k = 2
+        """
+        l, r = 1, max(piles)
+        if len(piles) > h: return False
+        res = r
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / k)
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
+            else:
+                l = k + 1
+        return res
+
